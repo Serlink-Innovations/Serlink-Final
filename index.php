bfcce20fb4c587c20aaaace14be5db8834436fc0
Serlink-Innovations <serlink.business@outlook.com>
2022-12-29 21:20:22 -0400

    v1.6.8

--- index.php
+++ index.php
@@ -95,7 +95,12 @@
                     <button onclick="window.location.href='/projects.php'" name="Portfolio Button" class="btn">
                         Projects
 
-                        <?xml version="1.0" ?><svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><title/><g data-name="Layer 2" id="Layer_2"><path d="M22,9a1,1,0,0,0,0,1.42l4.6,4.6H3.06a1,1,0,1,0,0,2H26.58L22,21.59A1,1,0,0,0,22,23a1,1,0,0,0,1.41,0l6.36-6.36a.88.88,0,0,0,0-1.27L23.42,9A1,1,0,0,0,22,9Z"/></g></svg>
+                        <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                            <title />
+                            <g data-name="Layer 2" id="Layer_2">
+                                <path d="M22,9a1,1,0,0,0,0,1.42l4.6,4.6H3.06a1,1,0,1,0,0,2H26.58L22,21.59A1,1,0,0,0,22,23a1,1,0,0,0,1.41,0l6.36-6.36a.88.88,0,0,0,0-1.27L23.42,9A1,1,0,0,0,22,9Z" />
+                            </g>
+                        </svg>
 
                     </button>
 
