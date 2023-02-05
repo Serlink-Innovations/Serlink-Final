bdec6dff952e5b502aec3427c16a31f49c5ba608
Serlink-Innovations <serlink.business@outlook.com>
2023-02-05 00:39:12 -0400

    v1.7.3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,14 @@
 //Mobile HeaderFixed function
 function stickyMenu() {
+  const header = document.querySelector("body");
+  const navigation = document.querySelector(".navigation-float");
+  navigation.classList.toggle("sticky", window.scrollY > 0);
 
-    const navigation = document.querySelector(".navigation-float");
-    navigation.classList.toggle("sticky", window.scrollY > 0);
-
+  if (navigation.classList.contains("sticky")) {
+    header.style.paddingTop = "120px";
+  } else {
+    header.style.paddingTop = "0";
+  }
 }
 
 window.addEventListener("scroll", stickyMenu);
